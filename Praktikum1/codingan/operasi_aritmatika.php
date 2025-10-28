8cceda4f93be054b79de503606ada6e526acffe9
EkaBintangNabila <bintang39393@gmail.com>
2025-10-28 11:04:12 +0700

    Create operasi_aritmatika.php

---
+++ Praktikum1/codingan/operasi_aritmatika.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contoh Operator Aritmatika PHP</title>
+</head>
+<body>
+    <?php
+    $a = 10;
+    $b = 3;
+
+    echo "\$a = $a <br>";
+    echo "\$b = $b <br>";
+
+    echo '$a + $b = ';
+    print $a + $b;
+    echo "<br>";
+
+    echo '$a - $b = ';
+    print $a - $b;
+    echo "<br>";
+
+    echo '$a * $b = ';
+    print $a * $b;
+    echo "<br>";
+
+    echo '$a / $b = ';
+    print $a / $b;
+    echo "<br>";
+
+    echo '$a % $b = ';
+    print $a % $b;
+    ?>
+</body>
+</html>
