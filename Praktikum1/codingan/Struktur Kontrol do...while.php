47c439685e502434e0c5e2c9d87ce7a86a4a99d1
EkaBintangNabila <bintang39393@gmail.com>
2025-10-28 11:04:40 +0700

    Create Struktur Kontrol do...while.php

---
+++ Praktikum1/codingan/Struktur Kontrol do...while.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contoh Perulangan Do While di PHP</title>
+</head>
+<body>
+    <?php
+    $i = 2;
+    do {
+        echo "\$i = $i <br>";
+        $i++;
+    } while ($i < 5);
+    ?>
+</body>
+</html>
