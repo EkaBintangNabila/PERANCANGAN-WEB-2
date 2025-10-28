b70a04aa1e7632f889bf30df7065822a1d86b54f
EkaBintangNabila <bintang39393@gmail.com>
2025-10-28 11:04:33 +0700

    Create Struktur Kontrol  If - else.php

---
+++ Praktikum1/codingan/Struktur Kontrol  If - else.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contoh Struktur If-Else di PHP</title>
+</head>
+<body>
+    <?php
+    $a = 5;
+    $b = 3;
+
+    echo "\$a = $a <br>";
+    echo "\$b = $b <br>";
+
+    if ($a < $b) {
+        echo '$a lebih kecil daripada $b';
+    } else {
+        echo '$a lebih besar daripada $b';
+    }
+    ?>
+</body>
+</html>
