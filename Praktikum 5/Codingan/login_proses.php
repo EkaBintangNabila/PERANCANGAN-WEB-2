e673f67ebaa84cd7860870afe9b88c3dc3416147
EkaBintangNabila <bintang39393@gmail.com>
2025-11-25 14:04:48 +0700

    Create login_proses.php

---
+++ Praktikum 5/Codingan/login_proses.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+include "koneksi.php";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+
+    $query = "SELECT * FROM admin WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($conn, $query);
+
+    if(mysqli_num_rows($result) > 0){
+        $_SESSION['admin'] = $username;
+        header("Location: admin.php");
+        exit;
+    } else {
+        echo "<script>alert('Login gagal! Username atau password salah'); window.location='login.php';</script>";
+    }
+}
+?>
+
