caae6a649499c2f2896ec01c76d0858b546327b9
EkaBintangNabila <bintang39393@gmail.com>
2025-11-25 14:04:45 +0700

    Create login.php

---
+++ Praktikum 5/Codingan/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman Login Admin</title>
+</head>
+<body>
+<h2>Login Admin</h2>
+
+<form action="login_proses.php" method="POST">
+    <label>Username:</label><br>
+    <input type="text" name="username" required><br><br>
+
+    <label>Password:</label><br>
+    <input type="password" name="password" required><br><br>
+
+    <button type="submit">Login</button>
+</form>
+
+</body>
+</html>
