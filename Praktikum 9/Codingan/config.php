7d67e6bad6a8053f6ebb5442d58c50e2dbceb2fa
EkaBintangNabila <bintang39393@gmail.com>
2025-12-16 11:00:08 +0700

    “Menambahkan file praktikum”  “Upload tugas praktikum”  “Initial commit”

---
+++ Praktikum 9/Codingan/config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+  'smtp_host' => 'smtp.gmail.com',
+  'smtp_port' => 587,
+  'smtp_user' => 'bintang39393@gmail.com',
+  'smtp_pass' => 'zjupjpmzjubfjias',
+  'from_email' => 'bintang39393@gmail.com',
+  'from_name'  => 'Sistem Data Mahasiswa',
+  'to_email'   => 'sputraian597@gmail.com',
+  'to_name'    => 'Penerima'
+];
